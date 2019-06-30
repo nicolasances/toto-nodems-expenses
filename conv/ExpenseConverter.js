67f79dd17b8e717914089ffd95b05e9a5fcb8e6a
nicolas <nicolas.matteazzi@gmail.com>
2019-06-30 16:45:10 +0200

    Filtering on tags

--- conv/ExpenseConverter.js
+++ conv/ExpenseConverter.js
@@ -104,6 +104,8 @@ exports.filterExpenses = function(filter) {
     let tagName = "additionalData." + splittedFilter[0];
     let tagValue = splittedFilter[1];
 
+    console.log("{" + tagName + ": " + tagValue + "}");
+
     tagFilter = JSON.parse("{" + tagName + ": " + tagValue + "}");
 
   }
