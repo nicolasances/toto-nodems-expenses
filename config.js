91b17ca6f53b94d15e7580b6440f531db92089ba
nicolasances <nicolas.matteazzi@gmail.com>
2022-10-28 13:16:06 +0200

    Added logs

--- config.js
+++ config.js
@@ -7,12 +7,13 @@ exports.mongoUrl = "mongodb://" + process.env.MONGO_USER + ":" + process.env.MON
 
 exports.dbName = 'expenses';
 exports.collections = {
-    expenses: 'expenses', 
-    categories: 'categories', 
-    food: 'food', 
-    meals: 'meals', 
-    settings: 'settings', 
-    cron: 'cron'};
+    expenses: 'expenses',
+    categories: 'categories',
+    food: 'food',
+    meals: 'meals',
+    settings: 'settings',
+    cron: 'cron'
+};
 
 exports.exchangeRateUrl = 'https://v3.exchangerate-api.com/pair/4c53838ecdaca2a7f1849fb3'
 
@@ -27,31 +28,37 @@ class Config {
             let promises = [];
 
             promises.push(client.accessSecretVersion({ name: 'projects/' + process.env.GCP_PID + '/secrets/toto-client-id-google/versions/latest' }).then(([version]) => {
+
                 this.clientIDGoogle = version.payload.data.toString();
+
+                console.log(`Retrieved Google Client ID: ${this.clientIDGoogle}`);
             }));
 
             // promises.push(client.accessSecretVersion({ name: 'projects/' + process.env.GCP_PID + '/secrets/toto-client-id-firebase/versions/latest' }).then(([version]) => {
             //     this.clientIDFirebase = version.payload.data.toString();
             // }));
 
-            Promise.all(promises).then(success, failure);
+            Promise.all(promises).then(() => {
+                console.log("Configuration Loaded!");
+                success()
+            }, failure);
 
-        })
+    })
 
+}
+
+getProps() {
+    return {
+        noCorrelationId: false,
+        noAuth: false
     }
+}
 
-    getProps() {
-        return {
-            noCorrelationId: false, 
-            noAuth: false
-        }
+getAuthorizedClientIDs() {
+    return {
+        "google": this.clientIDGoogle
     }
-
-    getAuthorizedClientIDs() {
-        return {
-            "google": this.clientIDGoogle
-        }
-    }
+}
 }
 
 exports.config = new Config();
